cfcfd397996e873a55535480e73f268448463a14
KocherginIvan <150249047+KocherginIvan@users.noreply.github.com>
2024-09-05 14:40:17 +0300

    Update main.py

--- Kochergin_Ivan/main.py
+++ Kochergin_Ivan/main.py
@@ -1,11 +1,11 @@
 import moduls.model
 import sys
-from streamlit import cli as stcli
-
-
+from streamlit import __main__ as stcli
 
 model = moduls.model.edit_model()
 
+
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", 'C:\Project\Corn\streamlit_app.py']
+    sys.argv = ["streamlit", "run", 'C:\Project\Corn\streamlit_app.py', '--theme.base', 'dark', '--theme.primaryColor', '#F59A07', '--theme.font', 'serif']
     sys.exit(stcli.main())
+
